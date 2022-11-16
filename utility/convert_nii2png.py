2dc64106214b9f25dbef1485827b544377d3d884
Az-ai <57408946+ace-aitech@users.noreply.github.com>
2022-11-16 11:59:17 +0000

    Add files via upload

--- utility/convert_nii2png.py
+++ utility/convert_nii2png.py
@@ -2,55 +2,48 @@
 # -*- coding: utf-8 -*-
 
 """
-Created on Sun Aug  8 19:13:44 2021
-
 @author: aze_ace
 """
-
-#########################################################
-#This code is adapted from nii2png
-#https://github.com/alexlaurence/NIfTI-Image-Converter
-#########################################################
-
 
 import os
 import shutil
 
 import nibabel as nib
 import numpy as np
+import cv2
 import pylab
 
 
-def nii2png(input_path, output_path, idx_list, slice_ax='z'):
+def nii2png(input_path, output_path, slice_ax='z', remove=False):
     """
-    Converts 3D volumes to png images
-
-    Parameters
-    ----------
-    input_path : str
-        Input directory
-    output_path : str
-        Output directory
-    idx_list : list of random indices
-        list of random indices  to select volumes
-    slice_ax : string
+    :param input_path: str
+        Input directory to the nii volumes
+    :param output_path: str
+        Output directory to place the sliced volumes
+    :param slice_ax: string
         axis to slide a volume
-     slice 2D images and masks of nii volume
-    -------
-
+    :param remove:  boolean to remove the input directory
+    :return: slice 2D images and masks of nii volume
     """
+    assert slice_ax in ['x', 'y', 'z'], ' Only axes x, y and z available'
 
-    source_ct = list(sorted(os.listdir(os.path.join(input_path, 'CT'))))
-    source_gt = list(sorted(os.listdir(os.path.join(input_path, 'GT'))))
+    size = (512, 512)
+
+    source_ct = sorted(os.listdir(os.path.join(input_path, 'CT')))
+    source_gt = sorted(os.listdir(os.path.join(input_path, 'GT')))
     ct_dir = os.path.join(input_path, 'CT')
     gt_dir = os.path.join(input_path, 'GT')
 
     dst_ct = os.path.join(output_path, 'CT')
     dst_gt = os.path.join(output_path, 'GT')
 
+    assert len(source_ct) == len(source_gt), 'Unequal number of Images and Masks'
+
     i = 0  # counter
 
-    for idx in idx_list:
+    no_mask = 0
+
+    for idx in range(len(source_ct)):
 
         ct = source_ct[idx]
         gt = source_gt[idx]
@@ -60,88 +53,68 @@ def nii2png(input_path, output_path, idx_list, slice_ax='z'):
         img_array = nib.load(ct_fname).get_fdata()
         mask_array = nib.load(gt_fname).get_fdata()
 
+        print(mask_array.shape)
+
         ct.replace('nii.gz', '')
         gt.replace('nii.gz', '')
 
         if slice_ax == 'z' and mask_array.shape[2] == img_array.shape[2]:
-
-            total_slices = img_array.shape[2]
+            start = 0
+            total = img_array.shape[2]
 
         elif slice_ax == 'y' and mask_array.shape[1] == img_array.shape[1]:
-
-            total_slices = img_array.shape[1]
+            start = 246
+            total = img_array.shape[1] // 2 + 5
 
         elif slice_ax == 'x' and mask_array.shape[0] == img_array.shape[0]:
-
-            total_slices = img_array.shape[0]
+            start = 246
+            total = img_array.shape[0] // 2 + 5
 
         else:
-            break
+            continue
 
         # iterate through slices
-        for sl in range(0, total_slices):
-            # alternate slices
-            if (i % 1) == 0:
-                # rotate or no rotate
+        for sl in range(start, total):
 
-                if slice_ax == 'z':
-                    ct_data = img_array[:, :, sl]
-                    gt_data = mask_array[:, :, sl]
-                    if 'coronacases' in ct:
-                        ct_data = np.rot90(ct_data, k=1)
-                        gt_data = np.rot90(gt_data, k=1)
+            if slice_ax == 'z':
+                ct_data = img_array[:, :, sl]
+                gt_data = mask_array[:, :, sl]
 
-                    elif 'volume' in ct:
-                        ct_data = np.rot90(ct_data[0:500, 70:500], k=-1)
-                        gt_data = np.rot90(gt_data[0:500, 70:500], k=-1)
+            elif slice_ax == 'x':
+                ct_data = img_array[sl, :, :]
+                gt_data = mask_array[sl, :, :]
 
-                    elif 'radiopaedia' in ct:
-                        ct_data = np.rot90(ct_data, k=-1)
-                        gt_data = np.rot90(gt_data, k=-1)
+            else:
+                ct_data = img_array[:, sl, :]
+                gt_data = mask_array[:, sl, :]
 
-                elif slice_ax == 'x':
-                    ct_data = img_array[sl, :, :]
-                    gt_data = mask_array[sl, :, :]
+                # selection of image slices based on the mask
 
-                elif slice_ax == 'y':
-                    ct_data = img_array[:, sl, :]
-                    gt_data = mask_array[:, sl, :]
+            if ct_data.shape != size:
+                ct_data = cv2.resize(ct_data, size, interpolation=cv2.INTER_NEAREST)
+                gt_data = cv2.resize(gt_data, size, interpolation=cv2.INTER_NEAREST)
 
-                else:
-                    print('Axis not available')
-                    break
+            if len(np.unique(gt_data)) > 1:  # mask with covid
 
-                # selection of image slices based on the mask 
+                i += 1
+                img_name = f'ct_gz{i}{slice_ax}' + '{:0>3}'.format(str(sl + 1)) + '.png'
+                mask_name = f'gt_gz{i}{slice_ax}' + "{:0>3}".format(str(sl + 1)) + '.png'
 
-                print('Saving image...')
+                pylab.imsave(img_name, ct_data, cmap='gray')
+                pylab.imsave(mask_name, gt_data, cmap='gray')
 
-                img_name = 'ct_' + str(i) + slice_ax + '{:0>3}'.format(str(sl + 1)) + \
-                           '.png'
-                mask_name = 'gt_' + str(i) + slice_ax + "{:0>3}".format(str(sl + 1)) + \
-                            '.png'
+                # move images to folder
+                src_img = img_name
+                src_mask = mask_name
+                shutil.move(src_img, dst_ct)
+                shutil.move(src_mask, dst_gt)
 
-                if len(np.unique(gt_data)) != 1:  # mask with covid
-                    # imageio.imwrite(image_name, data) 
-                    pylab.imsave(img_name, ct_data, cmap='gray')
-                    pylab.imsave(mask_name, gt_data, cmap='gray')
+            else:
+                no_mask += 1
 
-                    print('Saved...')
+    if remove:
+        shutil.rmtree(ct_dir)
+        shutil.rmtree(gt_dir)
+    print(f'Imanges without mask {no_mask}')
 
-                    # move images to folder
-                    print('Moving image...')
-                    src_img = img_name
-                    src_mask = mask_name
-                    shutil.move(src_img, dst_ct)
-                    shutil.move(src_mask, dst_gt)
-                    i += 1
-                    print('Moved...')
-                    print('Finished converting images')
-                else:
-                    print('No mask')
-
-            print('Num of img and mask slices in axes {},:{}'.format("z", i))
-
-    
-
-
-
+    print(f'Num of img and mask slices in axis {slice_ax}: {i}')
